27848e1c666d1c1ee9069511821abab35b8e0d85
Facundo Sa <facundo.sa@web.com>
2018-09-21 17:40:32 -0300

    Product list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent],//this component belongs to this module 
+  declarations: [AppComponent, ProductListComponent],//this component belongs to this module 
   imports: [BrowserModule], //Every Angular application must import this, so the browsers can interpretate the angular modules. 
   bootstrap: [AppComponent] //Defines the startup component of the application. 
 })

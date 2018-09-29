7ede942fb713944fd34adf0131f5cb0f756003a5
Facundo Sa <facundo.sa@web.com>
2018-09-29 20:30:15 -0300

    Modules refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router'; //Declares the router directives 
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent, ProductDetailComponent, WelcomeComponent],//this component belongs to this module 
+  declarations: [AppComponent, WelcomeComponent],//this component belongs to this module 
   imports: [
     BrowserModule, //Every Angular application must import this, so the browsers can interpretate the angular modules. 
-    FormsModule, 
     HttpClientModule, 
     //by using hash style routing we indicate that is local routing and we don't need to perform a rewrite process in the webserver. 
     //Angular supports HTML5 routing (without #) and local one (/#/)
     //The order is important, the specific ones must be first, then the other ones. 
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent},
-      { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent},
       { path: 'welcome', component: WelcomeComponent},
       { path: '', component: WelcomeComponent, pathMatch: 'full'}, //Default route
       { path: '**', redirectTo: 'welcome', pathMatch: 'full'} //When the url does not match, it's often used for 404 pages
-    ], {useHash: false})], 
+    ], {useHash: false}), ProductModule], 
   bootstrap: [AppComponent] //Defines the startup component of the application. 
 })
 export class AppModule { }
